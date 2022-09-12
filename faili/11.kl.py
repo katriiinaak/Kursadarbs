2b79b2cf0dfe54874325f008c9a2ed884c88c057
katriiinaak <102228708+katriiinaak@users.noreply.github.com>
2022-09-12 14:46:59 +0300

    up

---
+++ faili/11.kl.py
@@ -0,0 +1,22 @@
+""""
+Sastādīt programmu Python vidē, kas pieprasa ievadīt 2 skaitļus un darbību (+, -, *, /).
+Atkarībā no izvēlētās darbības izvada atbildi.
+Katrai darbībai uzrakstīt rakstīt savu funkciju ar parametriem, kuras argumenti būs ievadītie skaitļi. 
+"""
+v = int(input("Īevadiet pirmo skaitli: "))
+k = int(input("Ievadiet otro skaitli: "))
+print("Ievadiet, kādu darbību vēlaties izpildīt?")
+t = int(input("Varianti: 1-saskaitīšana; 2-atņemšana; 3-reizināšana; 4-dalīšana: "))
+
+import funkcijas
+
+
+if t == 1:
+    r = funkcijas.saskaitit(v, k)
+    print(f"{v} + {k} = {r}")
+# elif t == 2:
+#     print(f"{v} - {k} = ", funkcijas.atnemt(v, k))
+# elif t == 3:
+#     print(f"{v} * {k} = ", funkcijas.reizinat(v, k))
+# else:
+#     print(f"{v} / {k} = ", funkcijas.dalit(v, k))
