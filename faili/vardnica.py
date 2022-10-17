b1a8109b2b75bd28b72824a7d81f34e456f7ec81
katriiinaak <102228708+katriiinaak@users.noreply.github.com>
2022-10-17 14:35:38 +0300

    stunda

--- faili/vardnica.py
+++ faili/vardnica.py
@@ -24,11 +24,28 @@ vārdnīca = {
 }
 
 
-for key, value in vārdnīca.items():
-    print(f"{key} - {value}")
+print("No kuras valodas vēlaties tulkot?")
+k = int(input("Atbilžu varianti: 1-Latviešu; 2-Angļu: " ))
 
+
+print("Dotie vārdi: ")
 for key in vārdnīca.keys():
     print(f"{key}")
 
-for value in vārdnīca.values():
-    print(f"{value}")
+
+
+
+v = str(input("Ievadiet vienu no dotajiem vārdiem, kuru vēlaties iztulkot: "))
+print(vārdnīca[v])
+
+print("Vai vēlaties iztulkot vēl kādu vārdu?")
+k = int(input("Atbilžu varianti: 1-Jā; 2-Nē: " ))
+
+if k == 1:
+    m = int(input("Cik vārdus vēlaties vēl iztulkot? ")) 
+    for i in range (m):
+        v = str(input("Ievadiet vienu no dotajiem vārdiem, kuru vēlaties iztulkot: "))
+        print(vārdnīca[v])
+    print("Paldies, ka izmantojāt tulkotāju! ")
+else:
+    print("Paldies, ka izmantojāt tulkotāju! ")
