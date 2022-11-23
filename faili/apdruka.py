77dee6a86540e4633518d4fbc2d4f48e57c1c477
katriiinaak <102228708+katriiinaak@users.noreply.github.com>
2022-11-23 13:29:27 +0200

    ok

--- faili/apdruka.py
+++ faili/apdruka.py
@@ -2,7 +2,7 @@
 Draugiem jāaprēķina, cik izmaksās apdrukātu T-kreklu pasūtīšana un piegāde, ņemot vērā, ka cena ir
 atkarīga no apdrukas veida un kreklu skaita, bet piegādes maksa ir atkarīga no pasūtījuma summas.
 """
-
+"""
 def aprekins(a, c):
     s = a * c
     print(“Summa: ” + s)
@@ -26,16 +26,17 @@ def izmaksas_receptei(recepte, cena):
       summa += daudzums * cena[sastavdala]
       return summa
 
-def izmaksas_kopa(abolu_svars, recepte, cenas):
+def izmaksas_kopa(aboli_svars, recepte, cenas):
 # Funkcija aprēķina izmaksas dotam daudzumam ābolu
 # izmantojot dotu recepti un cenu sarakstu
 # Izmaksas uz vienu kilogramu ābolu
     izmaksas_kg = izmaksas_receptei(recepte, cenas) / recepte[“aboli”]
 # Izmaksas uz doto daudzumu ābolu
-ievarijuma_izmaksas = abolu_svars * izmaksas_kg
+ievarijuma_izmaksas = aboli_svars * izmaksas_kg
 print(“Uz {} kg ābolu izmaksas būs {:.2f} EUR”.format(abolu_svars,
 ievarijuma_izmaksas))
 # Piemērs lietošanai
 # recepte1 = {“cukurs”: 0.6, “kanelis”: 0.008, “aboli”: 2.0, “udens”: 0.2}
 cenas1 = {“cukurs”: 0.75, “kanelis”: 30.0, “aboli”: 0.0, “udens”: 0.0}
 izmaksas_kopa(15.0, recepte1, cenas1)
+"""
